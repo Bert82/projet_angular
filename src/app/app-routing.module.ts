cd0106f6d7f8c8219f4e5aff994e493d11a05d92
Bert82 <menet.bertrand@gmail.com>
2023-04-20 19:35:41 +0200

    primeng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InscriptionPageComponent } from './inscription-page/inscription-page.component';
+import { EventPageComponent } from './event-page/event-page.component';
 
 const routes: Routes = [
   { path: 'inscription', component: InscriptionPageComponent },
+  { path: 'event', component: EventPageComponent },
 ];
 
 @NgModule({
-  declarations: [InscriptionPageComponent],
+  declarations: [InscriptionPageComponent, EventPageComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
--- src/app/inscription-page/inscription-page.component.ts
+++ src/app/inscription-page/inscription-page.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-inscription-page',
   templateUrl: './inscription-page.component.html',
-  styleUrls: ['./inscription-page.component.css']
+  styleUrls: ['./inscription-page.component.css'],
 })
-export class InscriptionPageComponent {
-
-}
+export class InscriptionPageComponent {}
